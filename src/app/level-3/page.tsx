76159e82c4c3f1ef4c82862287a144a1cf3a75b6
Ammar Vasee <ammarvasee@gmail.com>
2025-12-24 19:41:01 +0000

    make this default stock price  $41,670.92 (_for element <CardHeader>_)

--- src/app/level-3/page.tsx
+++ src/app/level-3/page.tsx
@@ -10,7 +10,7 @@ import { NewsEventPopup } from "@/components/NewsEventPopup";
 
 const cryptoAssetsConfig = [
     { id: "ZYNT", name: "ZYNTRA", price: 69410, volatility: 1.5 },
-    { id: "HEX", name: "HEXIUM", price: 88.12, volatility: 1.8 },
+    { id: "HEX", name: "HEXIUM", price: 41670.92, volatility: 1.8 },
     { id: "LEDG", name: "LEDGERA", price: 1050, volatility: 2.2 },
     { id: "CRYPT", name: "CRYPTONEX", price: 30, volatility: 2.5 },
 ];
